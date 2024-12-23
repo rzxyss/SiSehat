3a6e6dcf0f5ce13780a541ba672db3c636fec32f
rzxyss <rzkysa0@gmail.com>
2024-12-23 08:40:32 +0700

    add link regis

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,19 @@
 @extends('layouts.home')
 @section('home')
-    <div class="flex flex-row justify-evenly items-center w-full">
-        <div class="flex flex-col gap-5">
-            <h1 class="font-montserrat font-semibold text-6xl">SiSehat</h1>
-            <h1 class="font-montserrat font-semibold text-xl text-primary">Sistem Informasi Kesehatan</h1>
-            <p class="font-montserrat font-medium text-base text-ngrey">Ingin membuat jadwal temu dengan dokter, mencari
-                informasi tentang kesehatan?</p>
-            <div>
-                <a href="#"
-                    class="py-2 px-4 font-montserrat font-medium text-base bg-primary text-white rounded-md">Sign Up</a>
-            </div>
-        </div>
-        <div>
-            <img src="{{ asset('/assets/image/hero.png') }}">
+<div class="flex flex-row justify-evenly items-center w-full">
+    <div class="flex flex-col gap-1">
+        <h1 class="font-montserrat font-semibold text-6xl">SiSehat</h1>
+        <h1 class="font-montserrat font-semibold text-xl text-primary">Sistem Informasi Kesehatan</h1>
+        <p class="font-montserrat font-medium text-base text-ngrey">Ingin membuat jadwal temu dengan dokter, mencari
+            informasi tentang kesehatan?</p>
+        <div class="mt-2">
+            <a href="/register"
+                class="py-2 px-4 font-montserrat font-medium text-base bg-primary text-white rounded-md">Sign
+                Up</a>
         </div>
     </div>
+    <div>
+        <img src="{{ asset('/assets/image/hero.png') }}">
+    </div>
+</div>
 @endsection
