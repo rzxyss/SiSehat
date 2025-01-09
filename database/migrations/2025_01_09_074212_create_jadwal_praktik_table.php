a71d7b133c899e7b411cf4b48cfe7a6c484a6aef
Rizki Saepul Aziz <rzkysa0@gmail.com>
2025-01-09 14:48:38 +0700

    add table

---
+++ database/migrations/2025_01_09_074212_create_jadwal_praktik_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jadwal_praktik', function (Blueprint $table) {
+            $table->id();
+            $table->date('tanggal');
+            $table->time('jam_mulai');
+            $table->time('jam_selesai');
+            $table->unsignedBigInteger('id_dokter');
+            $table->foreign('id_dokter')->references('id')->on('users')->onUpdate('cascade')->onDelete('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jadwal_praktik');
+    }
+};
